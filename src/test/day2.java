9eacd34d38c5e9f3c11b88e6dbdcd28903250ead
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-12 17:10:11 +0530

    after test

--- src/test/day2.java
+++ src/test/day2.java
@@ -1,11 +1,20 @@
 package test;
 
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day2 {
 
+	@AfterTest
+	
+		public void posttest()
+		{
+			System.out.println("after test execution");
+	}
+	
+	
 	@Test
 	public void loan()
 	{
